c5ba2390c3bfcca373d827bd5359f4b4ffa17fa7
joyce <joycemchen@icloud.com>
2018-02-27 21:32:00 -0800

    Third Challenge
    Selecting note taking preferences via radio buttons.

---
+++ MyThirdChallenge/MyThirdChallenge/Default.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="MyThirdChallenge.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Your note taking preferences:<br />
+            <br />
+            <asp:RadioButton ID="PencilButton" runat="server" OnCheckedChanged="PencilButton_CheckedChanged" Text="Pencil" GroupName="WritingUtensils" />
+            <br />
+            <asp:RadioButton ID="PenButton" runat="server" OnCheckedChanged="PenButton_CheckedChanged" Text="Pen" GroupName="WritingUtensils" />
+            <br />
+            <asp:RadioButton ID="PhoneButton" runat="server" OnCheckedChanged="PhoneButton_CheckedChanged" Text="Phone" GroupName="WritingUtensils" />
+            <br />
+            <asp:RadioButton ID="TabletButton" runat="server" OnCheckedChanged="TabletButton_CheckedChanged" Text="Tablet" GroupName="WritingUtensils" />
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="OK" />
+            <br />
+            <br />
+            <asp:Label ID="ResultLabel" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ MyThirdChallenge/MyThirdChallenge/Default.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyThirdChallenge
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (PencilButton.Checked)
+            {
+                ResultLabel.Text = "You selected Pencil.";
+            }
+            else if (PenButton.Checked)
+            {
+                ResultLabel.Text = "You selected Pen.";
+            }
+            else if (PhoneButton.Checked)
+            {
+                ResultLabel.Text = "You selected Phone.";
+            }
+            else if (TabletButton.Checked)
+            {
+                ResultLabel.Text = "You selected Tablet.";
+            }
+        }
+
+        protected void PencilButton_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void PenButton_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void PhoneButton_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TabletButton_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
